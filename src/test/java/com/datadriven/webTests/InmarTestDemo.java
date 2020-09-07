1d5f33bb890f280db5bd0a9bf9a886f6f83dc11c
RK805 <33957750+RK805@users.noreply.github.com>
2020-09-08 01:04:43 +0530

    Update InmarTestDemo.java

--- src/test/java/com/datadriven/webTests/InmarTestDemo.java
+++ src/test/java/com/datadriven/webTests/InmarTestDemo.java
@@ -5,7 +5,6 @@ import static org.testng.Assert.assertEquals;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Random;
-
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import utilities.KeywordUtil;
